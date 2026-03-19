0f7383e09fb7032aef606591ddcac2c6b3d87fbc
tlb <dev@theloungebooth.com>
2026-03-19 23:59:26 +1000

    fix type scaling

--- app/components/sections/services/SectionHeroServices.tsx
+++ app/components/sections/services/SectionHeroServices.tsx
@@ -10,7 +10,7 @@ export function SectionHeroServices({ serviceType, heading, subheading, item, pr
       <FadeInGroup stagger={0.25} className="grid grid-cols-1 lg:grid-cols-2 gap-y-6 md:gap-y-8 gap-x-16 items-center">
         <FadeIn direction="up">
           {serviceType && (
-            <p className="mb-4 md:mb-6 type-base md:type-base-plus text-primary text-center md:text-left border-b border-primary mx-auto md:mx-0 w-fit">
+            <p className="mb-4 md:mb-6 type-base md:text-base-plus text-primary text-center md:text-left border-b border-primary mx-auto md:mx-0 w-fit">
               {serviceType}
             </p>
           )}
@@ -30,7 +30,13 @@ export function SectionHeroServices({ serviceType, heading, subheading, item, pr
         <FadeIn direction="up">
           <div className="aspect-4/5 w-full overflow-hidden bg-surface/65 rounded-custom">
             {item && (
-              <MediaItem item={item} sizes="(max-width: 1024px) 90vw, 50vw" widths={[400, 640, 900, 1280]} loading="eager" fetchPriority="high" />
+              <MediaItem
+                item={item}
+                sizes="(max-width: 1024px) 90vw, 50vw"
+                widths={[400, 640, 900, 1280]}
+                loading="eager"
+                fetchPriority="high"
+              />
             )}
           </div>
         </FadeIn>
